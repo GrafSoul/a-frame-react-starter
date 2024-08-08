add73651d7c627171b9ce475c715b87e9eb7527f
Colibrity Development <development@colibrity.com>
2024-08-08 16:01:06 +0500

    fix: Updated project

--- src/ARScene.jsx
+++ src/ARScene.jsx
@@ -1,18 +1,15 @@
 import 'aframe';
-import 'ar.js';
 
 const ARScene = () => {
   return (
-    <a-scene embedded vr-mode-ui="enabled: false" arjs="sourceType: webcam; debugUIEnabled: false;">
-      <a-marker preset="hiro">
-        <a-camera-static>
-          <a-cursor
-            gltf-model="https://grafsoul.github.io/a-frame-react-starter/photo-camera.glb"
-            position="0 0 -1"
-            scale="2 2 2"
-            animation="property: rotation; to: 0 360 0; loop: true; dur: 5000"></a-cursor>
-        </a-camera-static>
-      </a-marker>
+    <a-scene>
+      <a-camera>
+        <a-cursor
+          gltf-model="https://grafsoul.github.io/a-frame-react-starter/photo-camera.glb"
+          position="0 0 -1"
+          scale="2 2 2"
+          animation="property: rotation; to: 0 360 0; loop: true; dur: 5000"></a-cursor>
+      </a-camera>
     </a-scene>
   );
 };
