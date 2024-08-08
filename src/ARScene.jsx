62ba5566aac0177dbd6476e4de91241b63f84597
Colibrity Development <development@colibrity.com>
2024-08-08 15:24:06 +0500

    feat: Updated project

--- src/ARScene.jsx
+++ src/ARScene.jsx
@@ -1,37 +1,15 @@
-import { useEffect } from 'react';
 import 'aframe';
 
 const ARScene = () => {
-  useEffect(() => {}, []);
-
   return (
-    <a-scene
-      vr-mode-ui="enabled: false"
-      embedded
-      renderer="logarithmicDepthBuffer: true; antialias: true;"
-      background="color: #ECECEC"
-      environment="preset: forest"
-      loading-screen="enabled: true"
-      debug="true"
-      stats="true"
-      inspector="true"
-      device-orientation-permission-ui="enabled: true"
-      arjs="sourceType: webcam;"
-      webxr="optionalFeatures: hit-test;">
-      <a-assets>
-        <a-asset-item id="gltfModel" src=""></a-asset-item>
-      </a-assets>
-
-      <a-camera position="0 1.6 0" look-controls="true"></a-camera>
-      <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-
-      <a-gltf-model src="#gltfModel" position="0 0 -3" scale="0.5 0.5 0.5"></a-gltf-model>
-
-      <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-      <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-
-      <a-sky color="#ECECEC"></a-sky>
+    <a-scene>
+      <a-camera>
+        <a-cursor
+          gltf-model="/photo-camera.glb"
+          position="0 0 -1"
+          scale="2 2 2"
+          animation="property: rotation; to: 0 360 0; loop: true; dur: 5000"></a-cursor>
+      </a-camera>
     </a-scene>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    // host: '0.0.0.0',
     port: 3000,
-    https: {
-      key: fs.readFileSync("./.cert/key.pem"),
-      cert: fs.readFileSync("./.cert/cert.pem"),
-    },
+    // https: {
+    //   key: fs.readFileSync('./.cert/key.pem'),
+    //   cert: fs.readFileSync('./.cert/cert.pem'),
+    // },
+  },
+  build: {
+    outDir: 'docs',
   },
 });
