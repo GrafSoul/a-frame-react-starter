2b676a903ef318cf42800cb29bc27ec48ff75349
Colibrity Development <development@colibrity.com>
2024-08-08 15:58:17 +0500

    feat: Updated project

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,7 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   server: {
-    // host: '0.0.0.0',
     port: 3000,
-    // https: {
-    //   key: fs.readFileSync('./.cert/key.pem'),
-    //   cert: fs.readFileSync('./.cert/cert.pem'),
-    // },
   },
   build: {
     outDir: 'docs',
